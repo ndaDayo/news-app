7863b06184a5fa0a472f2949860ee8ce1f3f9a38
hayato sugawara <hayato711@gmail.com>
2021-08-14 21:52:29 +0900

    add Webview dummy

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function ArticleScreen() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>nda!!text</Text>
+            <WebView
+                style={styles.container}
+                source={{uri: 'https://expo.dev'}}
+            />
         </SafeAreaView>
     );
 }

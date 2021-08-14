12d27394f41f130af2659df75501594bf4d5d334
hayato sugawara <hayato711@gmail.com>
2021-08-14 13:47:54 +0900

    add components

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
+import ListItem from './components/ListItem';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <View style={styles.itemContainer}>
-                <View style={styles.leftContainer}>
-                    <Image
-                        style={{width: 100, height: 100}}
-                        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-                    />
-                </View>
-                <View style={styles.rightContainer}>
-                    <Text numberOfLines={3} style={styles.text}>NdaDayo!!</Text>
-                    <Text style={styles.subText}>News subText</Text>
-                </View>
-            </View>
+            <ListItem />
         </View>
     );
 }
@@ -27,28 +16,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    itemContainer: {
-        height: 100,
-        width: "100%",
-        borderColor: "gray",
-        borderWidth: 1,
-        flexDirection: "row"
-    },
-    rightContainer: {
-        flex: 1,
-        flexDirection: 'column',
-        padding: 10,
-        justifyContent: 'space-between'
-    },
-    text: {
-        fontSize: 16,
-        color: "gray",
-    },
-    subText: {
-        fontSize: 12
-    },
-    leftContainer: {
-        width: 100,
-    },
+    }
 });

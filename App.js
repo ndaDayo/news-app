875d529485c975bde2ef2298dc774c654b4110f2
hayato sugawara <hayato711@gmail.com>
2021-08-21 17:56:58 +0900

    add dispatcher

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import {Provider} from 'react-redux';
-import store from '/store';
+import store from './store';
 
 export default function App() {
     return (
--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
-import WebView from 'react-native-webview';
+import {StyleSheet, SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {useDispatch} from 'react-redux';
+import {addClip, deleteClip} from '../store/actions/user';
 
 export default function ArticleScreen({route}) {
     const {article} = route.params;
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={() => (dispatch(addClip({clip: article})))}>
+                <Text style={{margin: 10, fontSize: 30}}>ADD</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => (dispatch(deleteClip({clip: article})))}>
+                <Text style={{margin: 10, fontSize: 30}}>DELETE</Text>
+            </TouchableOpacity>
             <WebView
                 style={styles.container}
                 source={{uri: article.url}}

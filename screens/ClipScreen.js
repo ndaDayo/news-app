e4360d79c6f287306f24a4e0b366232e894bf549
hayato sugawara <hayato711@gmail.com>
2021-08-21 21:15:18 +0900

    add useSelector

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, FlatList} from 'react-native';
+import {useSelector} from 'react-redux'
+import ListItem from "../components/ListItem";
 
-export default function ClipScreen({route}) {
+export default function ClipScreen({navigation}) {
+    const user = useSelector(state => state.user);
+    const {clips} = user;
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{margin: 10, fontSize: 30}}>Clip</Text>
+            <FlatList
+                data={clips}
+                renderItem={({item}) => (
+                    <ListItem
+                        title={item.title}
+                        author={item.author}
+                        imageUrl={item.urlToImage}
+                        onPress={() => navigation.navigate("Article", {article: item})}
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
         </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
